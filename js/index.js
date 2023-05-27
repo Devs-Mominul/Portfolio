3a023eaaa5973f153d48ebd2ff02f62a0ec6e1f6
Mominul Haque <webdesignermominulhaque@gmail.com>
2023-05-26 20:06:05 -0700

    portfolio

---
+++ js/index.js
@@ -0,0 +1,25 @@
+jQuery(document).ready(function() {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+});
+// (document).ready(function(){
+//     $('.preloader').delay(500).fadeOut();
+// })
+
+    $(".preloader").delay(500).fadeOut();
+$('.back2_top').click(function(){
+    $('html,body').animate({
+        scrollTop:0,
+    },1000)
+})
+$(window).scroll(function(){
+    var minar=$(this).scrollTop();
+    if(minar >20){
+        $('.back2_top').fadeIn();
+    }else{
+        $('.back2_top').fadeOut();
+
+    }
+})
